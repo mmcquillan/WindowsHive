28b0cc917f5656fbbeedc3efa46d3118a3599b85
Matt McQuillan <matt@mcquillan.org>
2012-09-24 21:34:12 -0400

    feedback and arguments classes

---
+++ Feedback.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MultiExec
+{
+    class Feedback
+    {
+
+        public static VerbosityLevel verbosity = VerbosityLevel.Verbose;
+
+        public static void Verbose(string write)
+        {
+            if (verbosity == VerbosityLevel.Verbose)
+                Console.WriteLine(write);
+        }
+
+        public static void Minimal(string write)
+        {
+            if (verbosity <= VerbosityLevel.Minimal)
+                Console.WriteLine(write);
+        }
+
+        public static void Error(string write)
+        {
+            Console.WriteLine(write);
+        }
+
+    }
+
+    enum VerbosityLevel
+    {
+        Verbose = 1,
+        Minimal = 2,
+        Silent = 3
+    }
+
+}
